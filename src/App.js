f73988e95c22ed3afbf123871ca69ebb82ecd1d8
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-21 19:51:32 +0200

    Setups tailwind css

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import Signup from './pages/auth/Signup';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
   );
 }
 
