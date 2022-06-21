4e32970bef1209a4623c87a0f7e895241ea73517
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-21 18:58:44 +0200

    Setup login & signup routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/auth/Login';
+import Signup from './pages/auth/Signup';
+
 function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
