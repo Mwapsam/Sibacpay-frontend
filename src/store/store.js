e072b89802a390ce946dad8294616232e2c6af9f
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-28 05:19:47 +0200

    Create redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import user from './user';
+import merchantSlice from './merchant';
 
 const store = configureStore({
   reducer: {
     user,
+    merchant: merchantSlice,
   },
 });
 
