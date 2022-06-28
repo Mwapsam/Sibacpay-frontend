30104ca54ed8f4c500710f87b578fec0fe07e91f
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-28 05:11:34 +0200

    Add merchant creation form

---
+++ src/components/FormAction.js
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+export default function FormAction({
+  handleSubmit,
+  type = 'Button',
+  text,
+}) {
+  return (
+    <>
+      {
+            type === 'Button'
+              ? (
+                <button
+                  type="submit"
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 mt-10"
+                  onSubmit={handleSubmit}
+                >
+
+                  {text}
+                </button>
+              )
+              : <></>
+        }
+    </>
+  );
+}
