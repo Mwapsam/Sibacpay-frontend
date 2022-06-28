6ba47ec9a877761ef05b7031e18406351e0430b3
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-28 05:18:21 +0200

    Create signup page

--- src/pages/auth/Signup.js
+++ src/pages/auth/Signup.js
@@ -1,81 +1,18 @@
-import { useDispatch, useSelector } from 'react-redux';
-import React, { useState } from 'react';
-import { signup } from '../../store/user';
+import Header from '../../components/Header';
+import Signup from '../../components/Signup';
 
-const Signup = () => {
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.user);
-  const [values, setValues] = useState({
-    username: '',
-    email: '',
-    password: '',
-  });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setValues((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(signup(values));
-  };
-
+export default function SignupPage() {
   return (
-    <div>
-      <div>
-        <h4>Create Account</h4>
-      </div>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="username">
-              Username
-            </label>
-            <input
-              id="username"
-              type="text"
-              value={values.username}
-              onChange={handleInputChange}
-              name="username"
-              placeholder="Username"
-            />
-          </div>
-          <div>
-            <label htmlFor="email">
-              Email
-            </label>
-            <input
-              id="email"
-              type="text"
-              value={values.email}
-              onChange={handleInputChange}
-              name="email"
-              placeholder="Email"
-            />
-          </div>
-          <div>
-            <label htmlFor="password">
-              Password
-            </label>
-            <input
-              id="password"
-              type="password"
-              value={values.password}
-              onChange={handleInputChange}
-              name="password"
-              placeholder="Password"
-            />
-          </div>
-          <button type="submit">Signup</button>
-        </form>
-        {user && user.errors && <p>{user.errors}</p>}
+    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8 shadow-lg shadow-[#b8b9b9] p-11 rounded-xl border border-slate-300">
+        <Header
+          heading="Signup to create an account"
+          paragraph="Already have an account? "
+          linkName="Login"
+          linkUrl="/login"
+        />
+        <Signup />
       </div>
     </div>
   );
-};
-
-export default Signup;
+}
