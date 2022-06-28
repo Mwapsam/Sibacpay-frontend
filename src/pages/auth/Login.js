b821c61d45320225837d46e53cbc5d36d3ce9892
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-28 05:17:55 +0200

    Create login page

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,67 +1,18 @@
-import { useDispatch, useSelector } from 'react-redux';
-import React, { useState } from 'react';
-import { login } from '../../store/user';
+import Header from '../../components/Header';
+import Login from '../../components/Login';
 
-const Login = () => {
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.user);
-  const [values, setValues] = useState({
-    email: '',
-    password: '',
-  });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setValues((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(login(values));
-  };
-
+export default function LoginPage() {
   return (
-    <div>
-      <div>
-        <h4>Login</h4>
-      </div>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="email">
-              Email
-            </label>
-            <input
-              id="email"
-              type="text"
-              value={values.email}
-              onChange={handleInputChange}
-              name="email"
-              placeholder="Email"
-            />
-          </div>
-          <div>
-            <label htmlFor="password">
-              Password
-            </label>
-            <input
-              id="password"
-              type="password"
-              value={values.password}
-              onChange={handleInputChange}
-              name="password"
-              placeholder="Password"
-            />
-          </div>
-          <button type="submit">Login</button>
-        </form>
-        {user && user.error && <p>{user.error}</p>}
+    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8 shadow-lg shadow-[#b8b9b9] p-11 rounded-xl border border-slate-300">
+        <Header
+          heading="Login to your account"
+          paragraph="Don't have an account yet? "
+          linkName="Signup"
+          linkUrl="/signup"
+        />
+        <Login />
       </div>
     </div>
   );
-};
-
-export default Login;
+}
